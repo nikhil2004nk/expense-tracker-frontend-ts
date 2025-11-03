89062b51ed55fd3787dfc3e088a2361d87a2ae37
nikhil02kushwaha <nikhil.kushwaha@trishvifintech.com>
2025-11-03 17:51:17 +0530

    Refactor ErrorBoundary component import statement for improved type clarity

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from 'react'
+import { Component, type ReactNode } from 'react'
 import { XCircleIcon, ArrowPathIcon } from '@heroicons/react/24/outline'
 
 interface Props {
